46dc75575c3344b27db30aa365a5ef8fb28b6dcc
anovis <anovis@ymail.com>
2021-03-07 18:36:08 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Autosize Text – Figma plugin
+
+Welcome 👋
+
+This is the home for  ['Autosize Text' Figma Plugin](https://www.figma.com/community/plugin).
+
+
+
+## Issues
+
+Please file any issues, bugs or feature requests as an issue on our [GitHub](https://github.com/anovis/autosizetext/issues) page.
+
+## Want to Contribute
+
+If you would like to contribute to the plugin (e.g. by improving the documentation, solving a bug or adding a cool new feature) submit a [pull request](https://github.com/anovis/autosizetext/pulls).
+
+## Want to Support
+
+<!-- markdownlint-disable MD033 -->
+<a href="https://www.buymeacoffee.com/austennovis" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-blue.png" alt="Buy Me A Coffee" height="41" width="174"></a>
+<!-- markdownlint-disable MD033 -->
---
+++ code.ts
@@ -0,0 +1,65 @@
+//This plugin resizes text to fit in its textbox
+
+const nodes: SceneNode[] = figma.currentPage.findAll(node => node.type === "TEXT")
+
+
+async function loadFont(fontName){
+  await figma.loadFontAsync(fontName)
+}
+
+nodes.forEach((node =>{
+  if (node.type === 'TEXT'){
+    loadFont(node.fontName).then(
+        ()=>resizeText(node)
+      )
+  }
+}))
+
+function resizeText(node: TextNode){
+  const _width = node.width
+  const _height = node.height
+  node.textAutoResize = "WIDTH_AND_HEIGHT"
+  var _currentWidth = node.width
+  var _currentHeight = node.height
+
+  if (_currentWidth > _width && _currentHeight > _height){
+    shrinkText(node, _width, _height)
+  }
+  else{
+    growText(node, _width, _height)
+  }
+}
+
+function shrinkText(node: TextNode, desiredWidth: number, desiredHeight: number){
+  var currentWidth = node.width
+  var currentHeight = node.height
+
+  while (currentWidth > desiredWidth || desiredHeight > currentHeight){
+    if (typeof(node.fontSize) === "number"){
+      node.fontSize -= 1
+    }
+    else{
+      // non standard font sizes
+      break
+    }
+    currentWidth = node.width
+  }
+}
+
+function growText(node: TextNode, desiredWidth: number, desiredHeight: number){
+  var currentWidth = node.width
+  var currentHeight = node.height
+
+  while (currentWidth < desiredWidth && currentHeight < desiredHeight){
+    if (typeof(node.fontSize) === "number"){
+      node.fontSize += 1
+    }
+    else{
+      // non standard font sizes
+      break
+    }
+    currentWidth = node.width
+  }
+}
+
+figma.closePlugin();
